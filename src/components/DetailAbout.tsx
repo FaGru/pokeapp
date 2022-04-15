e1bdcdb553b3146323d380499d715396ffd91b58
Tim Eifler <tim.eif@protonmail.com>
2022-04-15 17:50:13 +0200

    Improve fetch with loading/error state and add more styling to the layout

--- src/components/DetailAbout.tsx
+++ src/components/DetailAbout.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { PokemonRootObject } from '../interfaces/interfaces';
 import { SpeciesPokemonRootObject } from '../interfaces/species_interface';
 import axios from 'axios';
+import loadingSpinner from '../images/loadingSpinner.svg';
+import { FetchErrorButton } from './Buttons';
 
 export const DetailAbout: React.FC<{ pokemon: PokemonRootObject }> = ({
   pokemon,
@@ -10,61 +12,89 @@ export const DetailAbout: React.FC<{ pokemon: PokemonRootObject }> = ({
   const [pokemonSpeciesDetails, setPokemonSpeciesDetails] =
     useState<SpeciesPokemonRootObject | null>(null);
 
+  const [error, setError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
   useEffect(() => {
     const fetchData = async () => {
-      const { data }: any = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`
-      );
+      setLoading(true);
+      const { data }: any = await axios
+        .get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`)
+        .catch(error => {
+          setLoading(false);
+          setError(true);
+        });
+      setLoading(false);
       setPokemonSpeciesDetails(data);
-      //{ ERROR STATE?
     };
-    console.log('hello');
+
     fetchData();
-  }, []);
-  console.log(pokemonSpeciesDetails);
+  }, [pokemon.id]);
+
+  const handleReload = () => {
+    window.location.reload();
+    setError(false);
+  };
+
+  if (loading === true) {
+    return (
+      <LoadingContainer>
+        <img src={loadingSpinner} alt="loading..." height="80" width="80"></img>
+      </LoadingContainer>
+    );
+  }
+
   return (
-    <Container>
-      <p>
-        Bulbasaur can be seen napping in bright sunlight. There is a seed on its
-        back. By soaking up the sun's rays, the seed grows progressively larger.
-      </p>
-      <h4>Pokédex Data</h4>
-      <InfoContainer>
-        <Infobox>
-          <p>Species</p>
-          <p>
-            <span>{}</span>
-          </p>
-        </Infobox>
-        <Infobox>
-          <p>Height </p>
-          <p>
-            <span>{pokemon.height / 10}m </span>
-          </p>
-        </Infobox>
-        <Infobox>
-          <p>Weight </p>
-          <p>
-            <span>{pokemon.weight}kg</span>
-          </p>
-        </Infobox>
-        <Infobox>
-          <p>Abilities</p>
-          <p>
-            <span>
-              {pokemon.abilities[0].ability.name} <br />
-              {pokemon.abilities[1].ability.name}
-            </span>
-          </p>
-        </Infobox>
-        <Infobox>
-          <p>Weaknesses</p>
-          <p>
-            <span>Weaknesses</span>
-          </p>
-        </Infobox>
-      </InfoContainer>
-    </Container>
+    <>
+      {error ? (
+        <>
+          <h3 style={{ color: 'red', margin: '5px' }}>
+            Please reload there is an errror
+          </h3>
+          <FetchErrorButton onClick={handleReload}>RELOAD</FetchErrorButton>
+        </>
+      ) : (
+        <Container>
+          <p>{pokemonSpeciesDetails?.flavor_text_entries?.[6].flavor_text}</p>
+          <h4>Pokédex Data</h4>
+          <InfoContainer>
+            <Infobox>
+              <p>Species</p>
+              <p>
+                <span>{pokemonSpeciesDetails?.genera[7].genus}</span>
+              </p>
+            </Infobox>
+            <Infobox>
+              <p>Height </p>
+              <p>
+                <span>{pokemon.height / 10}m </span>
+              </p>
+            </Infobox>
+            <Infobox>
+              <p>Weight </p>
+              <p>
+                <span>{pokemon.weight}kg</span>
+              </p>
+            </Infobox>
+            <Infobox>
+              <p>Abilities</p>
+              <p>
+                <span>
+                  {pokemon.abilities[0].ability.name} <br />
+                  {pokemon.abilities[1]?.ability.name}
+                </span>
+              </p>
+            </Infobox>
+            <Infobox>
+              <p>Weaknesses</p>
+              <p>
+                <span>Weaknesses</span>
+              </p>
+            </Infobox>
+          </InfoContainer>
+        </Container>
+      )}
+    </>
   );
 };
 
@@ -94,4 +124,13 @@ const InfoContainer = styled.div`
 
 const Infobox = styled.div`
   display: flex;
+`;
+const LoadingContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transform: -webkit-translate(-50%, -50%);
+  transform: -moz-translate(-50%, -50%);
+  transform: -ms-translate(-50%, -50%);
 `;
