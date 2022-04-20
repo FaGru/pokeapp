f1804c21984c1f67970be7e862ceb121e5115af1
Fabian Gruber <fabian.gruber93@gmx.de>
2022-04-20 12:05:14 +0200

    Update detailabout test

--- src/components/DetailAbout.test.tsx
+++ src/components/DetailAbout.test.tsx
@@ -2,9 +2,22 @@ import DetailAbout from './DetailAbout';
 import { MemoryRouter } from 'react-router-dom';
 
 import { render, screen } from '@testing-library/react';
-import { testPokedetails } from './TestProps';
+import { testPokedetails, testPokemonTypeDetails } from './TestProps';
+import useStore from '../hooks/useStore';
+
+//Write test data into pokemonTypeDetails in Store
+useStore.getState().pokemonTypeDetails = testPokemonTypeDetails;
 
 describe('DetailAbout', () => {
+  it('renders all type images', () => {
+    render(
+      <MemoryRouter>
+        <DetailAbout pokemon={testPokedetails} />
+      </MemoryRouter>
+    );
+    const squareImagesElement = screen.getAllByTestId('square-image');
+    expect(squareImagesElement.length).toBe(8);
+  });
   it('tests the backgroundcolor of the headline', () => {
     render(
       <MemoryRouter>
