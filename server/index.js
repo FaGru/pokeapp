37525fd1c538a9e4f82f23448cf8d7c4ec56e0ff
Tim Eifler <tim.eif@protonmail.com>
2022-05-13 18:45:54 +0200

    Add API Routes for users

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const UserModel = require("./models/users");
+
 const dotenv = require("dotenv");
+const port = process.env.PORT || 3001;
 
 dotenv.config();
 const { MONGODB_URL } = process.env;
-
 app.use(express.json()); //converts automatically to json
 
 try {
@@ -16,26 +16,8 @@ try {
   console.error("ERROR: could not connect", error.message);
 }
 
-
-app.get("/getUsers", (req, res) => {
-  UserModel.find({}, (err, result) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-app.post("/createUser", async (req, res) => {
-  const user = req.body;
-  const newUser = new UserModel(user);
-  await newUser.save();
-
-  res.json(); //send user back to frontend
+app.listen(port, () => {
+  console.log(`SERVER RUNS FINE on port ${port}`);
 });
 
-let db = mongoose.connection;
-
-app.listen(3001, () => {
-  console.log("SERVER RUNS FINE");
-});
+app.use("/users", require("./routes/userRoutes"));
--- server/models/Users.js
+++ server/models/Users.js
@@ -15,6 +15,5 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-
-const UserModel = mongoose.model("users", UserSchema)
-module.exports = UserModel
+const UserModel = mongoose.model("users", UserSchema);
+module.exports = UserModel;
