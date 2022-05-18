c23e21e7b1291afaf1aa4d10b2a7b956ce8337b7
Tim Eifler <tim.eif@protonmail.com>
2022-05-18 16:26:45 +0200

    Add Authentification and password hasing for user registation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,50 @@
 const UserModel = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 // @desc    Register new User
 // @route   POST /users
 // @access  Public
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  //Check if user exists already by email
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invaild user Data");
+  }
+
   res.json({ message: "Register User" });
 });
 
