defe31815a0e31d092a43ea0928cbd0df92d10d4
Tim Eifler <tim.eif@protonmail.com>
2022-06-06 15:27:03 +0200

    test url

--- client/src/hooks/backendUseStore.tsx
+++ client/src/hooks/backendUseStore.tsx
@@ -26,7 +26,7 @@ const backendUseStore = create<backendInterface>((set, get) => ({
   userLoginInformation: userLoginInformation,
   isError: '',
   isLoading: false,
-  API_URL: 'https://pokeapp-backend-8b98f5ko0-fagru.vercel.app/users/',
+  API_URL: 'https://pokeapp-backend-8b98f5ko0-fagru.vercel.app/users',
 
   register: async (formData: Object) => {
     set({ isLoading: true });
