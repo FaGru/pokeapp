20cbc66c3efb4380360e4e1103289100eb4f197c
Tim Eifler <tim.eif@protonmail.com>
2023-01-17 07:32:33 +0100

    Update index.tsx
    Refactor

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
 
 import reportWebVitals from './reportWebVitals';
 
-/**
- *  @returns {JSX.Element}: returns Homepage
- */
+const container = document.getElementById('root');
 
-const container: HTMLElement | null = document.getElementById('root');
 if (!container) {
-  throw new Error('Failed to find root document');
+throw new Error('Failed to find root document');
 }
-const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+const Root = createRoot(container);
+
+const render = (): void => {
+Root.render(
+<React.StrictMode>
+<BrowserRouter>
+<GlobalStyle />
+<App />
+</BrowserRouter>
+</React.StrictMode>
 );
+};
 
+render();
 reportWebVitals();
+
+export default render;
